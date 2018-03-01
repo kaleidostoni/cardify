383235bf51c1b44cde8f7128966908fd3a789dcb
kaleidostoni <kaleidostoni@gmail.com>
2018-02-28 21:32:17 -0600

    subiendo funcion para agregar elemento figure

--- js/app.js
+++ js/app.js
@@ -1,9 +1,27 @@
 //Aqui tramos el elemento html 
 let gallery = document.getElementById('gallery');
-console.log(gallery);
+//console.log(gallery);
 
-const myGallery = parametro =>{
-    let a = Array.from(parametro);
-    console.log(a);
+
+const myGallery = htmlElement =>{
+ const photos = htmlElement.getElementsByTagName('img');
+ console.log(photos);
+ //volviendo la colección de html en arreglo
+ const photoArray = Array.from(photos);
+console.log(photoArray);
+//iterando en los elementos del array para colocarlos dentro de su etiqueta figure
+photoArray.map(function(photo){
+const image = document.createElement('img');
+const figure = document.createElement('figure');
+const figCapt = document.createElement('figcaption');
+
+image.src = photo.src;
+figCapt.innerHTML= photo.alt;
+
+figure.appendChild(image);
+figure.appendChild(figCapt);
+gallery.appendChild(figure);
+})
 }
 myGallery(gallery);
+//window.addEventListener('click', myGallery);
